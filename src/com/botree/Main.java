3b2ea756dcbeecd8d1e06ccd94ef5981029f8563
praveena <praveenapraveena13253@gmail.com>
2024-11-25 15:52:16 +0530

    push

--- src/com/botree/Main.java
+++ src/com/botree/Main.java
@@ -18,6 +18,8 @@ public class Main {
 		System.out.println(Main.add(56, 23));
 		System.out.println(Main.add(86, 456));
 		System.out.println(Main.add(56, 43));
+		
+		System.out.println("push");
 	}
 
 }
