ee74569c2d706347abfdd21a21571d7706f9df32
praveena <praveenapraveena13253@gmail.com>
2024-11-25 14:15:33 +0530

    First Commit

---
+++ src/com/botree/Main.java
@@ -0,0 +1,23 @@
+package com.botree;
+
+public class Main {
+
+	int a,b;
+	
+	public static int add(int a,int b)
+	{
+		return a+b;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("The Addition Result are");
+		System.out.println(Main.add(3, 2));
+		System.out.println(Main.add(12, 34));
+		System.out.println(Main.add(56, 23));
+		System.out.println(Main.add(86, 456));
+		System.out.println(Main.add(56, 43));
+	}
+
+}
