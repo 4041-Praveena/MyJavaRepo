fce16ecb7af8dd507224a80b9d6eb85c646ded19
praveena <praveenapraveena13253@gmail.com>
2024-11-25 15:18:19 +0530

    multi commit

---
+++ src/com/botree/nithya/NithyaTest.java
@@ -0,0 +1,10 @@
+package com.botree.nithya;
+
+public class NithyaTest {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Hello from Vasu");
+	}
+
+}
---
+++ src/com/botree/vasu/VasuTest.java
@@ -0,0 +1,10 @@
+package com.botree.vasu;
+
+public class VasuTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello from Vasu");
+	}
+
+}
